b0abcadab9669a957166da83df03decf6bb80125
German III <germaniiifelisarta@gmail.com>
2021-05-04 14:39:43 +0800

    v0.1 Added Credit Card Arrays and simple Error Trapping, also made css modular

--- card_checker.php
+++ card_checker.php
@@ -1,16 +1,23 @@
 <?php
-        $value_celsius = 0;
+        $credit_card_numbers = array("1234-5678-9101-1213", "1234 - 5678 - 9101 - 1213", "1234567891011213", "1234  5678  9101  1213",  // these are valid inputs
+                                    "1234  5678  9101", "1234-5678 -9101 -1213");   // these are invalid inputs
+        $array_counter = 0;
+        $validity = "";
         echo "<table>
                 <tr>
-                    <td>Celsius</td>
-                    <td>Fahrenheit</td>
+                    <td>Credit Card Number</td>
+                    <td>Validity</td>  
                 </tr>
-
                 <tr>";
-                    for($value_celsius = 0; $value_celsius <= 100; $value_celsius++){
-                        $value_fahrenheit = ((9/5) * $value_celsius) + 32;
-                                echo "<td>",$value_celsius, "</td>";
-                                echo "<td>", $value_fahrenheit, "</td></tr><tr>";
+                    for($array_counter; $array_counter < 6; $array_counter++){
+                        echo "<td>",$credit_card_numbers[$array_counter], "</td>";          // print the card number column
+                        
+                        //checks if card number is correct
+                        if(strlen($credit_card_numbers) >= 16){                             // checks if there is enough characters in the string which is 16
+                            
+                        }else $validity = "Invalid"
+
+                        echo "</tr>";
                     }
 
         echo "</table>";
