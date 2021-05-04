8e554d00a7668270964592cd2ab73f4bcb455750
German III <germaniiifelisarta@gmail.com>
2021-05-04 16:50:21 +0800

    v0.1 Added card_checker.php

--- felisarta_71.php
+++ felisarta_71.php
@@ -1,14 +1,14 @@
 <!FELISARTA, German III E  16101002 CpE 3222>
 <html>
 <head>
-    <title> 6-2 What's My Pin? </title>
+    <title> 6-2 What's my Card Number Again? </title>
     <meta name = "keywords" content = "German, Felisarta, PHP, Practice"> 
     <meta name = "description" content = "This webpage is a practice page for PHP functions">
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 
 <body>
-    <h1>6-2 What's My Pin?</h1>
+    <h1>6-2 What's my Card Number Again?</h1>
     <h2>German E Felisarta III | 16101002 | CpE 3222</h2>
     <?php include 'isvalid.php'?>
 
