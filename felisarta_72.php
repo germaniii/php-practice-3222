9fdd5b43fc191bf42679c3ec16b34090a6186475
German III <germaniiifelisarta@gmail.com>
2021-05-05 13:54:56 +0800

    v0.0 Added Multiplication Function

--- felisarta_72.php
+++ felisarta_72.php
@@ -1,19 +1,19 @@
 <!FELISARTA, German III E  16101002 CpE 3222>
 <html>
 <head>
-    <title> 6-2 What's my Card Number Again? </title>
+    <title> 7-2 Multiplication Table</title>
     <meta name = "keywords" content = "German, Felisarta, PHP, Practice"> 
     <meta name = "description" content = "This webpage is a practice page for PHP functions">
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 
 <body>
-    <h1>6-2 What's my Card Number Again?</h1>
+    <h1>7-2 Multiplication Table</h1>
     <h2>German E Felisarta III | 16101002 | CpE 3222</h2>
 
-    <form action="card_checker.php" method="post">
-        <label for="cc_num">Input Credit Card Number: </label>
-        <input type="text" value="input here"name="cc_num" id="cc_num"><br><br>
+    <form action="mult_table.php" method="post">
+        <label for="n">Display Mult Table From 1 to :</label>
+        <input type="text" value="1"name="n" id="n"><br><br>
         <input type="submit" value="Submit">
     </form>
 
---
+++ mult_table.php
@@ -0,0 +1,39 @@
+<!DOCTYPE php>
+<html>
+<head>
+    <title> 7-2 Multiplication Table</title>
+    <meta name = "keywords" content = "German, Felisarta, PHP, Practice"> 
+    <meta name = "description" content = "This webpage is a practice page for PHP functions">
+    <link rel="stylesheet" type="text/css" href="styles.css">   
+</head>
+<body>
+<?php
+        $input = $_POST['n'];
+        $x = 1;
+        $y = 2;
+        $validity = "";
+
+        //Error Trapping, should be numeric
+        if(is_numeric($input)){
+            //Error Trapping, input should be whole number
+            if(ctype_digit(strval($input))){
+                echo "  <h1> Multiplication Table of $input </h>
+                        <table>";
+
+                for($x; $x <= $input; $x++){         // main loop, prints the row
+                    echo "<tr><td>", $x, "</td>";     
+
+                        for($y; $y <= $input; $y++){    // prints the column
+                            $mult = $x * $y;
+                            echo "<td>", $mult, "</td>";
+                        }
+                    $y=2;
+                    echo "</tr>";
+                }
+
+                echo "</table>";
+            }else echo "<h2>Input Should be a Whole Number!</h2>";
+        }else echo "<h2>Input Should be a number! </h2>";
+?>
+</body>
+</html>
